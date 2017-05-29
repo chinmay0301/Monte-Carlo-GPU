fa403808071909adb5d1585c9c8e07e85e72200b
Chinmay Talegaonkar <chinmay0301@gmail.com>
2017-05-29 21:16:44 +0530

    Files for pi estimation benchmarking
    test.cu - takes around 0.571 seconds to run
    montecarlo.cpp takes around 13 seconds to run, 
    for 540000000 simulations. (This is a fairly small simulation, but still reasonable speed up)

---
+++ montecarlo.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<math.h>
+#include<ctime>
+using namespace std;
+
+
+int main () {
+
+long int n=300*300*6000;
+long int n_ok =0;
+float x,y;
+
+srand(time(0));
+
+for (long int i=0;i<n;i++)
+{
+x = rand();
+x = x/RAND_MAX;
+//srand(time(0)); 
+y = rand();
+y = y/RAND_MAX;
+
+if(sqrt(x*x + y*y) <=1)
+n_ok++;
+}
+float pi = 4.0 * n_ok/n;
+cout<<"pi is roughly " <<pi;
+return 0;
+}
+                                                                                                                                                                                                                                                                                          
+                                                                                                                           
+
